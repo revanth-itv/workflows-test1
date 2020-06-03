9a156793e35f1d435ce24db83e4dbe244bf96bf8
Re-vanth <revanth@itversity.in>
2020-06-03 16:42:10 +0530

    test 8

--- trial2.sh
+++ trial2.sh
@@ -9,3 +9,14 @@ else
 	cd /home/revanth/rick && git pull origin master
 fi
 
+
+
+if (( cd /home/revanth/rick && '.git/' in ls -Fa | grep -q .git/ & 'workflows-test1' in cd /home/revanth/rick && git config --get remote.origin.url ))
+then
+	cd /home/revanth/rick && git pull origin master
+else
+	cd /home/revanth/rick && git init
+	cd /home/revanth/rick && git remote add origin git@github.com:revanth-itv/workflows-test1.git
+	cd /home/revanth/rick && git pull origin master
+fi
+
